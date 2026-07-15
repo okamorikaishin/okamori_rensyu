ca6ba07323dd4a3f047b4f31f38de5d72a3f6e98
sakuma <k.sakuma@kbit.co.jp>
2026-07-15 15:29:02 +0900

    動作環境の確立

--- src/janken/Player.java
+++ src/janken/Player.java
@@ -1,5 +1,19 @@
 package janken;
 
 public class Player {
+    private String name;
 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    // 手を選ぶロジック(人間ならコンソール入力、CPUならランダム)
+    public Hand chooseHand() {
+        // 実装はAさんにお任せ
+        return Hand.ROCK; // 仮
+    }
+
+    public String getName() {
+        return name;
+    }
 }
