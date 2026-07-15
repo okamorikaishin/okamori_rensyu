31b17061255140cc62ff64aa4b9314027096d77e
sakuma <k.sakuma@kbit.co.jp>
2026-07-15 16:24:32 +0900

    Mainの編集をしました

--- src/janken/Main.java
+++ src/janken/Main.java
@@ -1,23 +1,44 @@
 package janken;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+    		Scanner sc = new Scanner(System.in);
         Player player1 = new Player("Player1");
         Player player2 = new Player("Player2");
         Game game = new Game();
         ScoreBoard scoreBoard = new ScoreBoard();
+        
+        System.out.println("==========================================");
 
         // 例: 5回勝負
-        for (int i = 0; i < 5; i++) {
-            Hand hand1 = player1.chooseHand();
-            Hand hand2 = player2.chooseHand();
+        for (int i = 1; i < 6; i++) {
+        		System.out.println("~"+i+"回戦~");
+        		System.out.println();
+        		System.out.println("1:グー, 2:チョキ, 3:パー");
+        		System.out.print("どの手を出す?：");
+        		int number = sc.nextInt();
+        		System.out.println();
+            Hand hand1 = player1.chooseHand(number);
+            Hand hand2 = player2.chooseHand(number);
 
             int result = game.judge(hand1, hand2);
             scoreBoard.record(result);
 
             System.out.println(player1.getName() + ": " + hand1 + " vs " + player2.getName() + ": " + hand2);
+            
+            System.out.println();
+            
+            System.out.println("　　　　　　 DRAW");
+            
+            System.out.println();
+            
+            System.out.println("==========================================");
+            
         }
 
         scoreBoard.display();
+        sc.close();
     }
 }
