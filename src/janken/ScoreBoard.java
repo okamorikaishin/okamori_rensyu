f974b031d2461fca423f3fad97d76b07c5f7792c
test <test@test.com>
2026-07-15 16:24:38 +0900

    scoreboard編集

--- src/janken/ScoreBoard.java
+++ src/janken/ScoreBoard.java
@@ -6,14 +6,23 @@ public class ScoreBoard {
     private int draws = 0;
 
     public void record(int result) {
-        if (result == 1) player1Wins++;
-        else if (result == 2) player2Wins++;
-        else draws++;
+        if (result == 1) {
+          	player1Wins++;
+        }else if (result == 2) {
+         	player2Wins++;
+        }else {
+         	draws++;
+        }
     }
 
     public void display() {
+        int total = player1Wins + player2Wins + draws;
         System.out.println("Player1: " + player1Wins + "勝");
         System.out.println("Player2: " + player2Wins + "勝");
         System.out.println("引き分け: " + draws + "回");
+        if (total > 0) {
+            double winRate = (player1Wins / total) * 100;
+            System.out.println(winRate);
+        }
     }
 }
