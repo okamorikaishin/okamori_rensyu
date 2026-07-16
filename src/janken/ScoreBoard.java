12b4b25d23005dc368a5cc8c866dee90df3dd9a8
test <test@test.com>
2026-07-16 10:39:08 +0900

    ScoreBoard.javaに連勝数、勝率を計算するメソッドを使い

--- src/janken/ScoreBoard.java
+++ src/janken/ScoreBoard.java
@@ -1,5 +1,19 @@
 package janken;
 
 public class ScoreBoard {
+    private int player1Wins = 0;
+    private int player2Wins = 0;
+    private int draws = 0;
 
+    public void record(int result) {
+        if (result == 1) player1Wins++;
+        else if (result == 2) player2Wins++;
+        else draws++;
+    }
+
+    public void display() {
+        System.out.println("Player1: " + player1Wins + "勝");
+        System.out.println("Player2: " + player2Wins + "勝");
+        System.out.println("引き分け: " + draws + "回");
+    }
 }
