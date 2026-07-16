de686152fa64dd3375d46afacd7d02f112eba2fe
sakuma <k.sakuma@kbit.co.jp>
2026-07-16 10:24:03 +0900

    メインを修正しました。

--- src/janken/Main.java
+++ src/janken/Main.java
@@ -9,12 +9,15 @@ public class Main {
         Player player2 = new Player("Player2");
         Game game = new Game();
         ScoreBoard scoreBoard = new ScoreBoard();
+        int con = 1;
+        int i =1;
         
         System.out.println("==========================================");
 
         // 例: 5回勝負
-        for (int i = 1; i < 6; i++) {
+        while (con==1) {
         		System.out.println("~"+i+"回戦~");
+        		i+=1;
         		System.out.println();
         		System.out.println("1:グー, 2:チョキ, 3:パー");
         		System.out.print("どの手を出す?：");
@@ -25,14 +28,36 @@ public class Main {
 
             int result = game.judge(hand1, hand2);
             scoreBoard.record(result);
-
+            
+            try {
+            	Thread.sleep(2000);
+            }catch(InterruptedException e){
+            		e.printStackTrace();
+            }
+            
             System.out.println(player1.getName() + ": " + hand1 + " vs " + player2.getName() + ": " + hand2);
             
             System.out.println();
             
+            try {
+            	Thread.sleep(500);
+            }catch(InterruptedException e){
+            		e.printStackTrace();
+            }
+            
             System.out.println("　　　　　　 "+game.judgeResultString(hand1, hand2));
             
             System.out.println();
+            
+            try {
+            	Thread.sleep(1500);
+            }catch(InterruptedException e){
+            		e.printStackTrace();
+            }
+            
+            System.out.println("　　　　　　　　　　　　　続けますか?");
+            System.out.print("　　　　　　　　　　　　　　やめる 0,続ける 1：");
+            con = sc.nextInt();
             
             System.out.println("==========================================");
             
